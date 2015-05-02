249c42092aa52ba1ad27d8af9305303cc00663af
Ryan Atkinson <ratkinson@zappos.com>
2015-05-01 21:14:12 -0700

    init, wrote up some pseudocodes

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # osreboot
+
+Scripts to help you setup your fresh os install.
+Dotfiles, settings, and download locations stored in
+a separate directory/project (defaults to ~/.dotfiles/)
+
+# dependencies
+
+## git
+
+Make sure you have git installed
+`git --version
+
+If not, install git http://git-scm.com/downloads
+
+## node.js
+
+Make sure you have node.js installed
+`node --version
+
+If not, install node.js https://nodejs.org/download/
---
+++ index.js
@@ -0,0 +1,27 @@
+/****  setup  ****/
+// find dotfiles
+  // check for ~/.dotfiles
+    // confirm using them
+    // prompt user for location?
+
+// prompt user for os and env
+
+// bkup each file as we create (not during setup)
+
+/****  rc files  ****/
+// if dir, prompt for variants
+  // use os & env from setup
+  // always use main+private
+  // prompt for anything else
+
+// setup bash_profile to load bashrc
+  // create new bash_profile? or append?
+
+/****  downloads  ****/
+// download (and execute?) all dmg's
+  // download to a local folder (tmp?) that goes in .gitignore
+    // check that folder before downloading dmg, zip, etc, to save time
+// download zip
+// open everything else in safari
+
+// can include callbacks
